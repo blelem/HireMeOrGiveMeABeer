5e45d2bb26ab31757d6021ea9c8c102050f87e4a
mansewiz <berthier_lemieux@hotmail.com>
2015-03-21 20:54:06 +0200

    Added local test app

--- WebProject2/Local_NoServer/Top.py
+++ WebProject2/Local_NoServer/Top.py
@@ -1,4 +1,7 @@
 #Used for debugging, when not in iPython
+import sys
+sys.path.append('app\\')
+
 import Alignment2D
 (kp1Matches, kp2Matches) = Alignment2D.SetupTheStuff()
 Alignment2D.LinearLeastSquare ( kp1Matches, kp2Matches ) 
