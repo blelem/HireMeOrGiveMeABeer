b0405baaa21f96d80011c8ce23cbd4075b1e6a36
jumppa <berthier_lemieux@hotmail.com>
2015-08-13 23:16:59 +0300

    Move AzureBlob to it's own class.

--- app/models.py
+++ app/models.py
@@ -1,18 +1,18 @@
-"""
+﻿"""
 Definition of models.
 """
 
 from django.db import models
+import azureImageProvider
 
 class InputImages(models.Model):
     set_name = models.CharField(max_length=200)
     image_1 = models.ImageField()
     image_2 = models.ImageField()
 
+    imageProvider = azureImageProvider.azureImageProvider()
+
     def image_1_url(self):
-        # Accessing directly the Azure blob storage. TODO: access via a model.
-        from azure.storage import BlobService
-        blob_service = BlobService(account_name='mansewiz', account_key='nexzj9VdFGQeTwagdnLOrGp4nXWNuNnnCJkFJbpqT58/A5iX0kdEqcUF0AGvxj9h0s7DXJdZruQGUj9KVldCpQ==')
-        strm = blob_service.make_blob_url('matchfeatures','myblob')
+        strm = self.imageProvider.getUrl('myblob')
         return strm;
 
